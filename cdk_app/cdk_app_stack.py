c919f08e57831b8c212785ec783b0aabc36bdc71
cherrydasika <cherry1311@gmail.com>
2023-12-31 15:46:18 +0000

    added 2 s3 buckets with local drive file uploads

--- cdk_app/cdk_app_stack.py
+++ cdk_app/cdk_app_stack.py
@@ -2,6 +2,8 @@ from aws_cdk import (
     # Duration,
     Stack,
     aws_s3 as s3,
+    aws_s3_deployment as s3Deploy,
+    RemovalPolicy,
     aws_lambda as lambda_,
     aws_iam as aws_iam,
     aws_glue as glue_,
@@ -15,13 +17,42 @@ class CdkAppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        glue_job_1_name="glue_script_test.py"  
+
         # Create an s3 buckets
-        s3.Bucket(
+        glue_bucket = s3.Bucket(
             self,
-            id="test_bucket_dev_personal_456-1",
-            bucket_name="test-bucket-cdk-input-datasets-2"
+            id="test_bucket_dev_personal_456-2",
+            bucket_name="test-bucket-cdk-input-datasets-3",
+            auto_delete_objects=True,
+            removal_policy=RemovalPolicy.DESTROY
 
         )
+
+        # Upload glue script to s3 bucket
+        glue_scripts = s3Deploy.BucketDeployment(self, 
+                                                 "glue-script-uploads", 
+                                                 sources=[s3Deploy.Source.asset('C:\D\github\cdk-app\glue')],
+                                                 destination_bucket=glue_bucket
+                                                )
+
+        
+        # Create a ouput bucket to hold csv files
+        csv_bucket = s3.Bucket(
+            self,
+            id="csv-output-1",
+            bucket_name="test-csv-ouput-1",
+            auto_delete_objects=True,
+            removal_policy=RemovalPolicy.DESTROY
+
+        )    
+
+        # Upload glue script to s3 bucket (csv file)
+        csv_uploads = s3Deploy.BucketDeployment(self, 
+                                                 "output_csv_files", 
+                                                 sources=[s3Deploy.Source.asset('C:\D\github\cdk-app\output_files')],
+                                                 destination_bucket=csv_bucket
+                                                )
 
         # First lambda function 
         hello_function = lambda_.Function(
@@ -32,6 +63,7 @@ class CdkAppStack(Stack):
         handler="hello.handler",
         timeout=Duration.seconds(60)
         )
+
 
         # Create a new role for glue
         glue_role = aws_iam.Role(self, 'glue-role',
@@ -49,7 +81,20 @@ class CdkAppStack(Stack):
                                      )
                                  }
 
-        )        
+        ) 
+
+        # Define the crawler
+        crawler = glue_.CfnCrawler(
+            self,
+            'csv-crawler',
+            role=glue_role.role_arn,
+            database_name='csv_db',
+            targets={
+                's3Targets': [{"path": f"s3://{csv_bucket.bucket_name}"}]
+            }
+
+
+        )       
 
         # Define the glue job
         glue_.CfnJob(self, 'my-glue-job', 
@@ -58,7 +103,7 @@ class CdkAppStack(Stack):
             command = glue_.CfnJob.JobCommandProperty(
                 name='pythonshell',
                 python_version='3.9',
-                script_location='s3://test-bucket-cdk-input-datasets-1/hello.py'
+                script_location=f's3://{glue_bucket.bucket_name}/{glue_job_1_name}'
             ),
             glue_version='3.0',
             timeout=2          
@@ -68,6 +113,6 @@ class CdkAppStack(Stack):
 
         # example resource
         # queue = sqs.Queue(
-        #     self, "CdkAppQueue",
+        #     self, "CdkAppQueue",git
         #     visibility_timeout=Duration.seconds(300),
         # )
---
+++ glue/glue_script_test.py
@@ -0,0 +1,3 @@
+print("Hellow world from CDK.")
+print("Wish you all happy new year")
+print("Look forward to a energetic start")
